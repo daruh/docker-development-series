e29f5dc2f8fda5c7c104bfa0b3e939910784d89e
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-02-23 13:59:22 +0100

    workshop fluentd

--- monitoring/logging/fluentd/introduction/file/app.sh
+++ monitoring/logging/fluentd/introduction/file/app.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-while true
-do
-	echo "Writing log to a file"
-  echo '{"app":"file-myapp"}' >> /app/example-log.log
-	sleep 5
+i=0; 
+while true;
+do 
+echo {\"time\":$(date +%s),\"msg\":\"This is my dummy log number ${i}\"}  >> /app/example-log.log ;
+ i=$((i+1)); 
+ sleep 1;
 done
